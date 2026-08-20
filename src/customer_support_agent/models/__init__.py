ac17ff437f42f7f52b96a3cb5ce4453d23404c6d
Cha Hoseong <hoseong@chahoseong.com>
2026-08-20 16:55:08 +0900

    feat: add OpenAI chat model text adapter

--- src/customer_support_agent/models/__init__.py
+++ src/customer_support_agent/models/__init__.py
@@ -1,3 +1,7 @@
 from customer_support_agent.models.base import ChatModel
+from customer_support_agent.models.openai import OpenAIChatModel
 
-__all__ = ["ChatModel"]
+__all__ = [
+    "ChatModel",
+    "OpenAIChatModel",
+]
