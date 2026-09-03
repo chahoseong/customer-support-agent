ceab7dd7806a8424dc6bd5dedcdb0948f6a607c1
Cha Hoseong <hoseong@chahoseong.com>
2026-09-03 16:10:09 +0900

    feat: connect order evaluation dataset evaluators

--- evals/order/dataset.py
+++ evals/order/dataset.py
@@ -2,16 +2,31 @@ from pathlib import Path
 
 from pydantic_evals import Dataset
 
-from customer_support_agent.agent import AgentResult
-
-from .models import OrderEvalInput, OrderEvalMetadata
+from .evaluators import (
+    ToolArgumentsEvaluator,
+    ToolOutcomesEvaluator,
+    ToolSelectionEvaluator,
+    ToolTrajectoryEvaluator,
+)
+from .models import (
+    OrderEvalInput,
+    OrderEvalMetadata,
+    OrderEvalOutput,
+)
 
 _DATASET_PATH = Path(__file__).with_name("scenario_cases.yaml")
 
 
-def load_order_dataset() -> Dataset[OrderEvalInput, AgentResult, OrderEvalMetadata]:
-    return Dataset[
+def load_order_dataset() -> Dataset[OrderEvalInput, OrderEvalOutput, OrderEvalMetadata]:
+    dataset = Dataset[
         OrderEvalInput,
-        AgentResult,
+        OrderEvalOutput,
         OrderEvalMetadata,
     ].from_file(_DATASET_PATH)
+
+    dataset.add_evaluator(ToolSelectionEvaluator())
+    dataset.add_evaluator(ToolArgumentsEvaluator())
+    dataset.add_evaluator(ToolOutcomesEvaluator())
+    dataset.add_evaluator(ToolTrajectoryEvaluator())
+
+    return dataset
